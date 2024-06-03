a022ffd1c1daf12f2d9f73fac14eb472c895ae34
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2024-06-03 13:51:37 +0100

    Try to fix

--- .github/get_minimal_version.py
+++ .github/get_minimal_version.py
@@ -4,7 +4,7 @@ import re
 # dependencies in order to ensure their compatibility during CI testing
 with open('./setup.py') as file:
     for line in file:
-        # version = re.search(r'(Core|France-Pension)\s*>=\s*([\d\.]*)', line)  # Use when proper core version will be used
-        version = re.search(r'(France-Pension)\s*>=\s*([\d\.]*)', line)
+        version = re.search(r'(Core|France-Pension)\s*>=\s*([\d\.]*)', line)  # Use when proper core version will be used
+        # version = re.search(r'(France-Pension)\s*>=\s*([\d\.]*)', line)
         if version:
             print(f'Openfisca-{version[1]}=={version[2]}')  # noqa: T201 <- This is to avoid flake8 print detection.
