fa25448c9d8901cdd6dc7c2e1da624dcbefca31d
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2024-06-06 17:27:23 +0100

    Fix YAML lint test

--- .github/lint-changed-yaml-tests.sh
+++ .github/lint-changed-yaml-tests.sh
@@ -2,7 +2,7 @@
 
 last_tagged_commit=`git describe --tags --abbrev=0 --first-parent`  # --first-parent ensures we don't follow tags not published in master through an unlikely intermediary merge commit
 
-if ! changes=$(git diff-index --name-only --diff-filter=ACMR --exit-code $last_tagged_commit -- "tests/*.yaml")
+if ! changes=$(git diff-index --name-only --diff-filter=ACMR --exit-code $last_tagged_commit -- "tests/formulas/*.yaml")
 then
   echo "Linting the following changed YAML tests:"
   echo $changes
