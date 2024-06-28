1df36f08c65b7a8fae18f66ae11291dcf83c90ce
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2024-06-28 18:13:35 +0100

    Test with new rebase core

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         ],
     install_requires = [
         "bottleneck >=1.3.2,<=2.0.0",
-        "OpenFisca-Core @ git+https://github.com/openfisca/openfisca-core@pension-rebased#egg=openfisca-core",
+        "OpenFisca-Core @ git+https://github.com/openfisca/openfisca-core@pension-rebase-2d#egg=openfisca-core",
         # "OpenFisca-Core >= 41.1,<42",
         "numba>=0.54,<1.0.0",
         "pandas>=2.0,<3.0",
