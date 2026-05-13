a425fdc3601a8d45aad00f9a1e69c37ce83cba0a
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2026-05-13 19:10:13 +0100

    Resserre la compatibilité OpenFisca-Core

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         ],
     install_requires = [
         "bottleneck >=1.3.2,<=2.0.0",
-        "OpenFisca-Core >= 41.5.0,<43.5",
+        "OpenFisca-Core >= 41.5.0,<43",
         "numba>=0.54,<1.0.0",
         "pandas>=2.0,<3.0",
         ],
